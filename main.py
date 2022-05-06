c18e5c3458ced70737d2c9a19f028c04cccf6e9a
locchuong <locchuong123@gmail.com>
2022-05-06 10:25:08 +0700

    add imgs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse 
 import numpy as np 
 import matplotlib.pyplot as plt
+from kmeans import KMeans
 
 # init parser
 parser = argparse.ArgumentParser(description = 'Use Kmeans to cluster color in the image')
@@ -9,6 +10,7 @@ parser = argparse.ArgumentParser(description = 'Use Kmeans to cluster color in t
 # create arguments
 
 # init model
+model = KMeans()
 
 if __name__  == '__main__':
     print('done!')
